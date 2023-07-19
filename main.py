ef0c6b6f9e08d5429758fe79310090f5aa8b2a17
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-07-19 21:39:31 +0800

    Add Automation

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-##################### Extra Hard Starting Project ######################
+import pandas as pd
+import datetime as dt
+import random
+import smtplib
 
-# 1. Update the birthdays.csv
+# Enter the Login Credentials
+my_email = "johnivanpuayapamerica@gmail.com"
+my_password = "xznqqbtxetfshwuy"
 
-# 2. Check if today matches a birthday in the birthdays.csv
+# Load the Date Today
+month = dt.datetime.now().month
+day = dt.datetime.now().day
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+# Load the Birthdays
+data = pd.read_csv("birthdays.csv")
+birthdays_today = data[(data['month'] == month) & (data['day'] == day)]
 
-# 4. Send the letter generated in step 3 to that person's email address.
+# Check if there are birthdays today
+if not birthdays_today.empty:
+    for index, row in birthdays_today.iterrows():
+        # Choose a message
+        with open(f"letter_templates\letter_{random.randint(1, 3)}.txt") as file:
+            message = file.read()
+            message = "Subject: Happy Birthday!\n\n" + message
+
+        # Change the Name
+        custom_message = message.replace("[NAME]", row['name'])
+
+        # Send the Email
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=my_password)
+            connection.sendmail(from_addr=my_email, to_addrs=row['email'], msg=custom_message)
 
 
-
-
+print(f"Sent {len(birthdays_today)} emails today")
